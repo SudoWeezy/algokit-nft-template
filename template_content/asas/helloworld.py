a9bdc949672e3f8c363f13b185253745986304fe
Stéphane <sudoweezy@gmail.com>
2023-04-03 18:24:48 +0200

    update template

--- template_content/asas/helloworld.py
+++
@@ -1,9 +0,0 @@
-import beaker
-import pyteal as pt
-
-app = beaker.Application("HelloWorldApp").apply(deploy_time_immutability_control).apply(deploy_time_permanence_control)
-
-
-@app.external
-def hello(name: pt.abi.String, *, output: pt.abi.String) -> pt.Expr:
-    return output.set(pt.Concat(pt.Bytes("Hello, "), name.get()))
